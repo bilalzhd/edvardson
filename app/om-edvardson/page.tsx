6d98dd986e2ebce35c2b80ec2e089a1a04cdf32d
bilalzhd <bilalzhd0@gmail.com>
2024-01-27 19:39:25 +0500

    add om edvardson

---
+++ app/om-edvardson/page.tsx
@@ -0,0 +1,60 @@
+export default function About() {
+    return (
+        <div>
+            Välkommen till Edvardson Sweden
+            Edvardson Sweden är ett anrikt familjeföretag, tidigare känt som Malungsfors Läderprodukter. Det började på 1860-talet då farfars farfar Edvard Torstensson började med skomakeri för att livnära sig. Sedan dess har tillverkningen utökats till att även innefatta jakttillbehör och friluftstillbehör i läder.
+
+            Hos oss sätter vi stort värde på kunskap, kvalitet och gediget hantverk.
+
+
+            Om Edvardson Sweden AB
+            God kvalité och bra funktion i allt vi gör, produkter som håller för strapatser och funktion som gör din upplevelse rikare. Vi har skapat de produkter vi själva vill använda och vi testar dem både på älgjakt och fjällvandring.
+
+            Bussarongen av vadmal ska hålla dig varm och torr på fågelskådning en tidig vårdag och det ska inte prassla när du smyger dig fram i terrängen.
+
+            Vi vill ha bästa tänkbara material för varje produkt och funktion. Materialen ska vara slitstarka, bekväma, tysta och lätta. Vi använder naturmaterial i första hand men ibland är det ett modernt syntetmaterial som har den specifika funktion som vi eftersträvar.
+
+            Dina synpunkter och erfarenheter är viktiga för oss och vi specialtillverkar gärna utifrån dina speciella önskemål.
+
+            Vi vill att våra produkter ska förhöja din upplevelse oavsett vad du har för hobby.
+
+            Profilprodukter
+            Ge dina kunder och anställda en unik och handgjord present.
+            Vi på Edvardson Sweden har en stor erfarenhet av profilprodukter och du är välkommen att bli vår nästa kund.
+
+            Historik
+            Vi i familjen Edvardsson har en lång tradition som skomakare. På 1860-talet kom farfars farfar vandrande från Norge och slog sig ner här i Malungsfors. Han försörjde sig som reparationsskomakare men lärde sig med tiden att göra slitstarka kängor och skor.
+
+            Under första hälften av 1900-talet utvecklades produkterna och yrkeskunnandet fördjupades. Tillverkningen flyttades till större lokaler och fler skinnkunniga bybor fick möjligheten att lära sig skomakeriets ädla konst. Malungsbygden har en lång skinntradition så känslan för skinnet och hur det ska hanteras var en nedärvd kunskap hos folket.
+
+            Numera tillverkar vi ytterplagg, jakttillbehör, vadmalskläder, vadmalsbyxor, vadmalsjackor, patronhållare, slutstyckshölster, bärsele för jaktradio och GPS, läderbälten, gevärsremmar och profilprodukter. Vi har numera ett så gediget kunnande så det är få saker vi inte vågar oss på. Vi kan med stolthet säga att våra läderprodukter är så slitstarka att de håller i åtskilliga år om du sköter dem väl.
+
+            Vi försöker i största möjliga mån att använda oss av vegetabiliskt garvat läder, med tanke på miljön.
+
+            Här hittar du oss:
+            Skinnargränd 2
+            782 75  MALUNGSFORS
+
+            070-3158536
+
+            Org.nr: 556833-3834
+
+            E-post: info@edvardson.se
+
+            Koordinater
+            WGS 84 (lat, lon):N 60° 44.005', E 13° 33.787'
+            WGS 84 decimal (lat, lon):60.73341, 13.56312
+            RT90:6737249, 1377692
+            SWEREF99:6733951, 421639
+
+
+            Länk till KARTA
+
+            Länk till försäljningsvillkor
+
+            Öppettider butik:
+            Varierande öppettider
+            Ring före besök. 070-3158536
+        </div>
+    )
+}
