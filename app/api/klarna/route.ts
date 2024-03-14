f92e2bd119eb0e68c5677754596e19851234207d
asajjad308 <51700302+asajjad308@users.noreply.github.com>
2024-03-14 13:31:14 +0500

    Update route.ts

--- app/api/klarna/route.ts
+++ app/api/klarna/route.ts
@@ -1,17 +1,49 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+ 
 
-// export async function GET() {
-//     return NextResponse.json({name: "Billa"})
-// }
+export async function GET(){
 
-export async function POST(req: NextRequest, res: NextResponse) {
-    let username = process.env.KLARNA_USERNAME;
-    let password = process.env.KLARNA_PASSWORD;
-    
+    return NextResponse.json("i am sajjad")
+}
+let username = "PK250113_0a0956f8edfc";
+let password = "tFAcWacQN4SzUNzq";
+ 
+
+export async function POST(req: any, res: any) {
     try {
-        const bodyData = req.body
+        const bodyData = {
+            locale: "sv-SE",
+            purchase_country: "SE",
+            purchase_currency: "SEK",
+            order_amount: 5000,
+            order_lines: [
+                {
+                    type: 'physical',
+                    reference: '1',
+                    name: 'Classic Low Bridge Sunglasses',
+                    color: 'White',
+                    size: 'Small',
+                    imgSrc: '/sunglasses2-min.jpg',
+                    quantity: 1,
+                    quantity_unit: 'pcs',
+                    unit_price: 5000,
+                    tax_rate: 0,
+                    total_amount: 5000,
+                    total_tax_amount: 0,
+                }
+            ],
+            intent: "buy",
+            merchant_urls: {
+                authorization: "https://edvardson.netlify.app/kassa?order=successful"
+            }
+        };
+
+     
+
+        // Encode username and password for Basic authentication
         const encodedAuth = btoa(`${username}:${password}`);
 
+        // Fetch API request
         const klarnaResponse = await fetch("https://api.playground.klarna.com/payments/v1/sessions", {
             method: 'POST',
             headers: {
@@ -26,10 +58,14 @@ export async function POST(req: NextRequest, res: NextResponse) {
         }
 
         const klarnaData = await klarnaResponse.json();
-        return NextResponse.json({ message: "Klarna session created successfully", data: klarnaData });
+        console.log('Klarna response:', klarnaData);
 
+        // Return Klarna response as JSON
+        return NextResponse.json({ message: "Klarna session created successfully", data: klarnaData });
     } catch (error) {
         console.error("Error processing POST request:", error);
+
+        // If an error occurs, send an appropriate error response
         return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
     }
 }
